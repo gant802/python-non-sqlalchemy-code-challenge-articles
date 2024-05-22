8b69ce9b0560f453f4d766f13451f2cf71487fad
Grant Cummings <grantcummings4429@gmail.com>
2024-05-22 08:50:42 -0400

    finished code challenege

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,156 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.add_article(self)
+
+    def __repr__(self):
+        return f"Article('{self.author}', '{self.magazine}', '{self.title}')"
+
+    @classmethod
+    def add_article(cls, new_article):
+        cls.all.append(new_article)
+
+    
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, new_author):
+        if isinstance(new_author, Author):
+            self._author = new_author
+        else : 
+            raise Exception("Customer must be an instance of the Customer class")
         
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, new_magazine):
+        if isinstance(new_magazine, Magazine):
+            self._magazine = new_magazine
+        else : 
+            raise Exception("Customer must be an instance of the Magazine class")
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str) and 5 <= len(title) <= 50 and not hasattr(self, "_title"):
+            self._title = title
+        else : 
+            raise Exception("Name must be a string between 5 and 50 characters")
+
 class Author:
     def __init__(self, name):
         self.name = name
 
-    def articles(self):
-        pass
+    def __repr__(self):
+        return f"Author('{self.name}')"
 
-    def magazines(self):
-        pass
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 0 < len(name) and not hasattr(self, "_name"):
+            self._name = name
+        else : 
+            raise Exception("Name must be a string more than 0 characters")
+        
+    def articles(self): #! Returns list of articles written by this author
+        return [article for article in Article.all if article.author == self]
 
-    def add_article(self, magazine, title):
-        pass
+    def magazines(self): #! Returns unique list of magazines the author has articles in
+        mag_set = {article.magazine for article in self.articles()}
+        return list(mag_set)
 
-    def topic_areas(self):
-        pass
+    def add_article(self, magazine, title):  #! Adds an article written by this author
+        return Article(self, magazine, title)
+
+    def topic_areas(self):  #! Returns unique list of the categories of magazines the author has contributed to
+        magazine_categories = {magazine.category for magazine in self.magazines()}
+        if len(magazine_categories) > 0:
+            return list(magazine_categories)
+        else : 
+            return None
 
 class Magazine:
+    all = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        Magazine.all.append(self)
 
-    def articles(self):
-        pass
+    def __repr__(self):
+        return f"Magazine('{self.name}', '{self.category}')"
 
-    def contributors(self):
-        pass
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 2 <= len(name) <= 16:
+            self._name = name
+        else : 
+            raise Exception("Name must be a string between 2 and 16 characters")
 
-    def article_titles(self):
-        pass
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, new_category):
+        if isinstance(new_category, str) and 0 < len(new_category):
+            self._category = new_category
+        else : 
+            raise Exception("Name must be a string more than 0 characters")
 
-    def contributing_authors(self):
-        pass
+    @classmethod
+    def add_new_magazine(cls, new_magazine):
+        cls.all.append(new_magazine)
+
+    def articles(self): #! Returns list of articles for this magazine
+        return [article for article in Article.all if article.magazine == self]
+
+    def contributors(self): #! Returns unique list of authors for this magazine
+        author_set = {article.author for article in self.articles()}
+        return list(author_set)
+
+    def article_titles(self): #! Returns unique list of article titles within this magazine
+        titles_set = {article.title for article in self.articles()}
+        if len(titles_set) > 0:
+            return list(titles_set)
+        else : 
+            return None
+
+    def contributing_authors(self): #! Returns list of authors that have more than 2 contributions to this magazine
+        list_of_authors = [article.author for article in self.articles()]
+        contributing_authors = [author for author in self.contributors() if list_of_authors.count(author) > 2]
+        if contributing_authors:
+            return contributing_authors
+        else : 
+            return None
+
+    @classmethod
+    def top_publisher(cls): #! Returns magazine instance with the most articles
+        is_articles = False
+        for magazine in cls.all:
+            if [article for article in Article.all if article.magazine == magazine]:
+                is_articles = True
+        if is_articles == True:
+            top_magazine_publisher = max(cls.all, key=lambda magazine: len(magazine.articles()))
+            return top_magazine_publisher
+        else : 
+            return None
+        
--- lib/debug.py
+++ lib/debug.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import ipdb;
+import ipdb
 
 from classes.many_to_many import Article
 from classes.many_to_many import Author
@@ -9,5 +9,19 @@ if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
 
-    # don't remove this line, it's for debugging!
-    ipdb.set_trace()
+Magazine.all = []
+Article.all = []
+# assert Magazine.top_publisher() == None
+
+author_1 = Author("Carry Bradshaw")
+magazine_1 = Magazine("Vogue", "Fashion")
+magazine_2 = Magazine("AD", "Architecture")
+# assert Magazine.top_publisher() == None
+
+Article(author_1, magazine_1, "How to wear a tutu with style")
+Article(author_1, magazine_1, "Dating life in NYC")
+Article(author_1, magazine_1, "How to be single and happy")
+Article(author_1, magazine_2, "2023 Eccentric Design Trends")
+Article(author_1, magazine_2, "Carrara Marble is so 2020")
+    
+ipdb.set_trace()
